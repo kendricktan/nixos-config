3184153d767fabdfde92b1cf4a6432e6aeb9765d
Kendrick Tan <kendricktan0814@gmail.com>
2018-09-12 17:14:40 +1000

    .

--- configuration.nix
+++ configuration.nix
@@ -313,6 +313,9 @@
     };
   };
 
+  # Security
+  security.sudo.wheelNeedsPassword = false;
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
